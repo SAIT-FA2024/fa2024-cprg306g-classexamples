4b35380c4bc2c23a55a8725951a04c13a4b14c4b
Derek Yadlowski <Derek.Yadlowski@sait.ca>
2024-10-03 11:09:21 -0600

    week 4 small update

--- app/week-4/counter-props/page.js
+++ app/week-4/counter-props/page.js
@@ -26,7 +26,7 @@ export default function CounterPropPage(){
                 resetCounterFunction={resetCounter}
             />
             <article>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at nisl sit amet lorem bibendum facilisis vitae vitae neque. Sed lobortis ligula non egestas sollicitudin. Donec mauris nibh, tincidunt eget ex vel, eleifend iaculis dolor. Curabitur dolor quam, posuere ultricies ligula vel, fringilla pharetra felis. Morbi eu nisi metus. Aenean a interdum augue. Nullam vel enim sed odio vestibulum fermentum ac a magna. Donec purus quam, vestibulum sed orci eu, porttitor ornare ante. Pellentesque ultrices, nunc quis volutpat ornare, libero augue scelerisque dui, id pretium lorem dolor aliquet quam. Sed eu sollicitudin nisl, eget maximus dolor. Nullam mi risus, euismod a ultricies non, euismod ac tortor. Nulla eu consequat elit, a tempus nunc. </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at nisl sit amet lorem bibendum facilisis vitae vitae neque.</p>
             </article>
             <CounterDisplay currentCount={counter} />
         </main>
